d6692a9965102fbf5e970e916e13d097829fca6c
Pitt009 <gmartinez28837@gmail.com>
2017-04-05 13:19:15 -0500

    Falta eliminar

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
 function agregarFruta(){
   var nombreFruta = document.getElementById('nombreFruta').value
   var frutaAgregada = document.createElement("li");
+  frutaAgregada.id= nombreFruta;
+  frutaAgregada.innerHTML= "<span onclick='eliminar(this)'>X</span>" + nombreFruta;
   lista.appendChild(frutaAgregada);
-  frutaAgregada.innerText = nombreFruta;
+  //frutaAgregada.innerText = nombreFruta;
   }
+
+
+function eliminar(elemento) {
+    var id=elemento.parentNode.getAttribute("id");
+    node=document.getElementById( id);
+    node.parentNode.removeChild(node);
+ }
